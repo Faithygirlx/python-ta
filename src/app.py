e70020ec18a1d3a9de9f231777f37a358ccba6e8
Faithygirlx <faitheyo16@gmail.com>
2022-06-19 17:39:52 +0100

    My solution

--- src/app.py
+++ src/app.py
@@ -1,13 +1,18 @@
 import sys
 import os
-
+import math
 
 def prime(s):
     # your code goes here
-
+    if s == 2 or s == 3:
+        return True
+    elif s % 2 == 0 or s % 3 == 0 or math.sqrt(s) == math.floor(math.sqrt(s)):
+        return False
+    else:
+        return True
+    
 def solution(s):
     return prime(s)
-
 
 if __name__ == "__main__":
     if len(sys.argv) <= 1:
